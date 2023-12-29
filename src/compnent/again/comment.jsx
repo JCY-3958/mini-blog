0ef617b7104f901525cc06f05feffc3e416d7281
JCY-3958 <w7e43958@naver.com>
2023-12-29 17:21:17 +0900

    first commit

---
+++ src/compnent/again/comment.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Comment() {
+  return (
+    <div>
+      <h1>이제야 조금 이해갈듯</h1>
+    </div>
+  );
+}
+
+export default Comment;
---
+++ src/compnent/again/commentList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Comment from "./comment";
+
+function CommentList() {
+  return (
+    <Comment/>
+  );
+}
